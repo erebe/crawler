01f4a8b291dc30d177239cc30775553cbc6ce73e
Erèbe <erebe@erebe.eu>
2014-04-27 21:50:03 +0200

    Add worker thread to fetch every 2hours

--- src/main.hs
+++ src/main.hs
@@ -6,6 +6,10 @@ import Control.Applicative((<$>))
 import Control.Monad(join)
 import Data.Maybe(fromMaybe)
 import Control.Concurrent.Async(async, wait)
+import Control.Concurrent.MVar(newEmptyMVar, MVar, putMVar, takeMVar)
+import Control.Monad(forever)
+import Control.Concurrent.Thread.Delay(delay)
+
 
 
 data Crawlable = Channels [Youtube.Channel]
@@ -19,19 +23,30 @@ loadConfigFile = do
     return $ read configFile
 
 
+spawnFetcher :: IO (MVar [Crawlable])
+spawnFetcher = do
+        fetchRes <- newEmptyMVar
+        _ <- async $ fetcher fetchRes
+        return fetchRes
+
+        where
+            getFromConfig key cfg = fromMaybe [] $ lookup key cfg
+            fetcher queue = forever $ do
+                    config <- loadConfigFile
+                    let actions = [ Series   <$> Eztv.fetchSeries (getFromConfig "eztv" config)
+                                  , Channels <$> Youtube.fetchChannels (getFromConfig "youtube" config)
+                                  ]  :: [IO Crawlable]
+
+                    jobs <- sequence $ async <$> actions
+                    res <- mapM wait jobs
+                    putMVar queue res
+                    putStrLn "Going to sleep !"
+                    delay (10^6 * 60 * 60 * 2)
+
 main :: IO ()
 main = do
-    -- args <- getArgs
-    config <- loadConfigFile
-    let actions = [ Series <$> Eztv.fetchSeries (getFromConfig "eztv" config)
-                  , Channels <$> Youtube.fetchChannels (getFromConfig "youtube" config)
-                  ]  :: [IO Crawlable]
-
-    jobs <- sequence $ async <$> actions
-    xs <- mapM wait jobs
-    print xs
-
-    where
-        getFromConfig key cfg = fromMaybe [] $ lookup key cfg
-
+    queue <- spawnFetcher
+    forever $ do
+        dest <- takeMVar queue
+        print dest
 
