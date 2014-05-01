0a693e5c25cbc7078ad88c5486775b09abc5e664
Erèbe <erebe@erebe.eu>
2014-05-01 22:07:15 +0200

    Stable version

--- src/main.hs
+++ src/main.hs
@@ -1,31 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 import qualified Eztv
 import qualified Youtube
 import System.Directory(getHomeDirectory)
 import Control.Applicative((<$>))
-import Control.Monad(join)
+import Control.Monad(join, forever)
 import Data.Maybe(fromMaybe)
 import Control.Concurrent.Async(async, wait)
-import Control.Concurrent.MVar(newEmptyMVar, MVar, putMVar, takeMVar)
-import Control.Monad(forever)
+import Control.Concurrent.MVar(MVar, newMVar, readMVar, swapMVar)
 import Control.Concurrent.Thread.Delay(delay)
 
 import Web.Scotty
-import Data.Monoid(mconcat)
 import Control.Monad.IO.Class(liftIO)
-import Control.Concurrent.MVar(swapMVar)
-import Control.Concurrent.MVar(readMVar)
-import Control.Concurrent.MVar(newMVar)
 import Data.List(find)
 import Control.Lens
+import qualified Data.ByteString.Lazy.Char8 as BC
 
 
-data Crawlable = Channels [Youtube.Channel]
-               | Series [Eztv.Serie]
+data Crawlable = Crawlable {
+                      _channels :: [Youtube.Channel]
+                     ,_series   :: [Eztv.Serie]
+                 } deriving (Show, Read)
 
-                deriving (Show, Read)
-
+$(makeLenses ''Crawlable)
 
 loadConfigFile :: IO [(String, [String])]
 loadConfigFile = do
@@ -33,48 +31,47 @@ loadConfigFile = do
     return $ read configFile
 
 
-spawnFetcher :: IO (MVar [Crawlable])
+spawnFetcher :: IO (MVar Crawlable)
 spawnFetcher = do
-        fetchRes <- newMVar []
+        fetchRes <- newMVar $ Crawlable [] []
         _ <- async $ fetcher fetchRes
         return fetchRes
 
         where
             getFromConfig key cfg = fromMaybe [] $ lookup key cfg
             fetcher queue = forever $ do
-                    config <- loadConfigFile
-                    let actions = [ Series   <$> Eztv.fetchSeries (getFromConfig "eztv" config)
-                                  , Channels <$> Youtube.fetchChannels (getFromConfig "youtube" config)
-                                  ] :: [IO Crawlable]
-
                     putStrLn "Start fetching !"
-                    jobs <- sequence $ async <$> actions
-                    res <- mapM wait jobs
+                    config     <- loadConfigFile
+                    series'    <- async $ Eztv.fetchSeries (getFromConfig "eztv" config)
+                    channels'  <- async $ Youtube.fetchChannels (getFromConfig "youtube" config)
+                    channels'' <- wait channels'
+                    series''   <- wait series'
                     putStrLn "Done fetching !"
-                    _ <- swapMVar queue res
-                    putStrLn "Going to sleep !"
-                    delay (10^6 * 60 * 60 * 2)
+
+                    _ <- swapMVar queue $ Crawlable channels'' series''
+                    delay (1000000 * 60 * 60 * 2)
 
 
-runRestServer queue = scotty 8080 $ do
-    get "/:word/:teo" $ do
-        word <- param "word" :: ActionM String
-        -- te <- param "teo"
-        dest <- liftIO $ readMVar queue
-        json $ show "" 
-                -- case word of
-                --         "serie" -> show $ case (fromMaybe (Series []) $ find (\a -> case a of
-                --                                                                Series _ -> True
-                --                                                                _ -> False) dest) of 
-                --                             Series a -> a ^.. Eztv.serieName
-                --                             _ -> []
-                --         _ -> ""
+runRestServer ::  MVar Crawlable -> IO ()
+runRestServer queue = scotty 8080 $ 
+    get "/:type/:val" $ do
+        crawlerType <- param "type" :: ActionM String
+        val <- param "val" :: ActionM String
+        crawl <- liftIO $ readMVar queue
+
+        let res = case crawlerType of
+                        "serie"  -> show $ fromMaybe (Eztv.Serie "" [])
+                                         $ find (\serie -> serie^.Eztv.serieName == val) (crawl^.series)
+                        "youtube" ->show $ fromMaybe (Youtube.Channel "" [])
+                                         $ find (\channel -> channel^.Youtube.name == val) (crawl^.channels)
+
+                        _        -> "tata"
+
+
+        raw $ BC.pack res
 
 main :: IO ()
 main = do
     queue <- spawnFetcher
     runRestServer queue
-    -- forever $ do
-    --     dest <- takeMVar queue
-    --     print dest
 
