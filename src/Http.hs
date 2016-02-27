b9a6e6e357599edd96c3b65d696042e496e9a947
Erèbe <romain.gerard@erebe.eu>
2016-02-28 00:04:29 +0100

    Remove deprecated

--- src/Http.hs
+++ src/Http.hs
@@ -1,38 +1,35 @@
+{-# LANGUAGE BangPatterns        #-}
 {-# LANGUAGE NoImplicitPrelude   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Http (getPages) where
 
 
-import           ClassyPrelude                hiding (replicate)
+import           ClassyPrelude             hiding (replicate)
 import           Data.List
 import           Network.HTTP.Conduit
 
 
-import           Control.Monad.Trans.Maybe    (MaybeT, runMaybeT)
-import           Control.Monad.Trans.Resource
-
-import           Control.Monad                (msum)
-
-import qualified Data.ByteString.Lazy         as BL
-
-import           System.IO                    hiding (hPutStrLn)
+import           Control.Monad             (msum)
+import           Control.Monad.Trans.Maybe (MaybeT, runMaybeT)
+import qualified Data.ByteString.Lazy      as BL
+import           System.IO                 hiding (hPutStrLn)
 
 
 getPages :: (BL.ByteString -> b) -> [String] -> IO [Maybe b]
-getPages func urls = withManager $ \m -> mapM (runWorker m) urls
+getPages func urls = newManager tlsManagerSettings >>= \m -> mapM (runWorker m) urls
     where
         runWorker m url = do pageBody <- runMaybeT $ worker m url
                              return $ func <$> pageBody
 
 
 
-worker :: Manager -> String -> MaybeT (ResourceT IO) BL.ByteString
+worker :: Manager -> String -> MaybeT IO BL.ByteString
 worker manager url = msum $ replicate 3 fetchPage
     where
       fetchPage = do
-          body <- liftIO $ fetchPageImpl `catchAny` \e -> do
-              hPrint stderr ((show e) <> " " <> url)
+          !body <- liftIO $ fetchPageImpl `catchAny` \e -> do
+              hPrint stderr (show e <> " " <> url)
               return BL.empty
 
           guard(body /= BL.empty)
